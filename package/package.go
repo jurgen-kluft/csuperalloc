60a38d0769c4e51803f62dd9918e4d6adf33ffec
Jurgen <jurgen.kluft@gmail.com>
2022-10-19 19:42:38 +0800

    * fix; package (remove cvmem)

--- package/package.go
+++ package/package.go
@@ -2,10 +2,9 @@ package csuperalloc
 
 import (
 	cbase "github.com/jurgen-kluft/cbase/package"
+	"github.com/jurgen-kluft/ccode/denv"
 	centry "github.com/jurgen-kluft/centry/package"
 	cunittest "github.com/jurgen-kluft/cunittest/package"
-	cvmem "github.com/jurgen-kluft/cvmem/package"
-	"github.com/jurgen-kluft/xcode/denv"
 )
 
 // GetPackage returns the package object of 'csuperalloc'
@@ -13,14 +12,12 @@ func GetPackage() *denv.Package {
 	// Dependencies
 	cunittestpkg := cunittest.GetPackage()
 	centrypkg := centry.GetPackage()
-	cvmempkg := cvmem.GetPackage()
 	cbasepkg := cbase.GetPackage()
 
 	// The main (csuperalloc) package
 	mainpkg := denv.NewPackage("csuperalloc")
 	mainpkg.AddPackage(cunittestpkg)
 	mainpkg.AddPackage(centrypkg)
-	mainpkg.AddPackage(cvmempkg)
 	mainpkg.AddPackage(cbasepkg)
 
 	// 'csuperalloc' library
@@ -32,7 +29,6 @@ func GetPackage() *denv.Package {
 	maintest.Dependencies = append(maintest.Dependencies, cunittestpkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, centrypkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, cbasepkg.GetMainLib())
-	maintest.Dependencies = append(maintest.Dependencies, cvmempkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, mainlib)
 
 	mainpkg.AddMainLib(mainlib)
