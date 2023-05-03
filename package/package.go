ce54a7f32892d36b51a719f32b076fd0a64cfd11
jurgen.kluft <jurgen.kluft@gmail.com>
2023-05-03 13:28:12 +0800

    * package; fix (removal of centry, not needed for cunittest)

--- package/package.go
+++ package/package.go
@@ -3,7 +3,6 @@ package csuperalloc
 import (
 	cbase "github.com/jurgen-kluft/cbase/package"
 	"github.com/jurgen-kluft/ccode/denv"
-	centry "github.com/jurgen-kluft/centry/package"
 	cunittest "github.com/jurgen-kluft/cunittest/package"
 )
 
@@ -11,13 +10,11 @@ import (
 func GetPackage() *denv.Package {
 	// Dependencies
 	cunittestpkg := cunittest.GetPackage()
-	centrypkg := centry.GetPackage()
 	cbasepkg := cbase.GetPackage()
 
 	// The main (csuperalloc) package
 	mainpkg := denv.NewPackage("csuperalloc")
 	mainpkg.AddPackage(cunittestpkg)
-	mainpkg.AddPackage(centrypkg)
 	mainpkg.AddPackage(cbasepkg)
 
 	// 'csuperalloc' library
@@ -27,7 +24,6 @@ func GetPackage() *denv.Package {
 	// 'csuperalloc' unittest project
 	maintest := denv.SetupDefaultCppTestProject("csuperalloc_test", "github.com\\jurgen-kluft\\csuperalloc")
 	maintest.Dependencies = append(maintest.Dependencies, cunittestpkg.GetMainLib())
-	maintest.Dependencies = append(maintest.Dependencies, centrypkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, cbasepkg.GetMainLib())
 	maintest.Dependencies = append(maintest.Dependencies, mainlib)
 
